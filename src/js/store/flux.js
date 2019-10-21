9236fff88797d6b60f1063bf4658607de0cf068a
Evan Otero <Evan.otero19@gmail.com>
2019-10-21 20:02:00 +0000

    blank functions created

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -7,7 +7,25 @@ const getState = ({ getStore, setStore }) => {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 			createcontact: () => {
-				const store = getStore();
+				let temp = blank;
+				let temp2 = { full_name: "$", email: "", phone: "", address: "" };
+				temp.push(temp2);
+				this.setstore((blank = temp2));
+			},
+			editcontact: index => {
+				let temp = blank[index];
+				temp.full_name = blank1;
+				temp.email = blank2;
+				temp.phone = blank3;
+				temp.address = blank4;
+				this.setstore((blank[index] = temp));
+			},
+			deletecontact: del => {
+				let temp = blank;
+				temp.filter((name, index) => {
+					del != index;
+				});
+				this.setStore((blank = temp));
 			}
 		}
 	};
